76f5bddc54bc399d9345fa65066d562cdeab4f0f
rehma <91306833+rehmaaaa@users.noreply.github.com>
2025-07-23 09:14:13 -0400

    Create config.py

---
+++ backend /config.py
@@ -0,0 +1,24 @@
+from datetime import timedelta
+
+
+class Config:
+    """
+    Configuration class for the Flask application.
+
+    Attributes:
+        SQLALCHEMY_DATABASE_URI (str): URI for the users database.
+        SQLALCHEMY_BINDS (dict): Dictionary of URIs for the image analysis database.
+        SQLALCHEMY_TRACK_MODIFICATIONS (bool): Whether to track modifications to the database.
+    """
+
+    # URI for the users database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+
+    # Dictionary of URIs for the image analysis database
+    SQLALCHEMY_BINDS = {
+        'image_analysis': 'sqlite:///image_analysis.db'
+    }
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)  # Token expires in 30 days
+    # Whether to track modifications to the database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
